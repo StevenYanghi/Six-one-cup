f4a1ed05047c193d54e0a281040b37abfeb3a7ac
StevenYanghi <137042229+StevenYanghi@users.noreply.github.com>
2023-08-20 23:29:44 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ function ad(){
     alert("-蒙猪牛奶，助力追梦!         -巴萨和牛，你的最优选！");
 }
 
+var slides = document.getElementsByClassName("Main");
+var currentSlide = 0;
+
+function showSlide() {
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].style.display = "block";
+}
+
+setInterval(showSlide, 3000);  //每隔3秒自动切换图片
