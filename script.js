f7f40692836771c7bbe6778e0c8e372adf020827
StevenYanghi <137042229+StevenYanghi@users.noreply.github.com>
2023-08-29 14:14:10 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function ad(){
 var slides = document.getElementsByClassName("slide");
 var currentSlide = 0;
 
-function showSlide() {
+/*function showSlide() {
   for (var i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
@@ -13,4 +13,35 @@ function showSlide() {
   slides[currentSlide].style.display = "block";
 }
 
-setInterval(showSlide, 3000);  //每隔3秒自动切换图片
+setInterval(showSlide, 3000);*/  //每隔3秒自动切换图片
+
+window.onscroll = function() {
+  toggleScrollTopButton();
+};
+
+function toggleScrollTopButton() {
+  var scrollTopBtn = document.querySelector('.scroll-top-btn');
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollTopBtn.classList.add('show');
+  } else {
+    scrollTopBtn.classList.remove('show');
+  }
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function loadNewContent() {
+  $.ajax({
+    url: 'new-content.html', // 新内容所在的文件或API端点
+    type: 'GET',
+    success: function(data) {
+      $('#content').html(data); // 将新内容替换原有内容
+    },
+    error: function() {
+      console.log('加载新内容失败');
+    }
+  });
+}
